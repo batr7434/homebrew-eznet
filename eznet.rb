d576318fe0e747e7630e86f0c94f69fbf1bb99e8
batr7434 <batr7434@gmail.com>
2025-10-21 13:54:37 +0200

    Update to use HEAD - latest CLI improvements with positional arguments

--- eznet.rb
+++ eznet.rb
@@ -1,10 +1,8 @@
 class Eznet < Formula
   desc "Comprehensive network testing CLI tool with port scanning and SSL certificate analysis"
   homepage "https://github.com/batr7434/eznet"
-  url "https://github.com/batr7434/eznet/archive/v0.2.0.tar.gz"
-  sha256 "24b8dae0bf455b446b5f9a53dbc44310c0241dd226ce80c7372246240b9a782b"
-  license "Apache-2.0"
   head "https://github.com/batr7434/eznet.git", branch: "main"
+  license "Apache-2.0"
 
   depends_on "python@3.12"
 
